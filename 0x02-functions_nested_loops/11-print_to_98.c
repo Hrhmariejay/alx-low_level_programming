2d0a49cce19fcdbcbd4dd5b38df92474f465e0b9
Hrhmariejay <mariejay230@gmail.com>
2023-04-26 08:10:06 -0700

    100

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,36 @@
  */
 void print_to_98(int n)
 {
-while (n < 98)
+if (n <= 98)
 {
-printf("%i, ", n);
-n++;
-}
-while (n > 98)
+for (; n <= 98; n++)
 {
-printf("%i, ", n);
-n--;
+if (n == 98)
+{
+printf("%d", n);
+printf('\n');
+break;
 }
-printf("98");
-_putchar('\n');
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf('\n');
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
 }
